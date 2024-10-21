0e6bf4863ad5da8c71166578c8f21efb7bed05b6
AlnurM <alnur.mustafayev@gmail.com>
2024-10-21 14:20:38 +0500

    fix: init stage & ellipse shape with points

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import { useEffect } from 'react';
+import { Stage, Layer, Shape, Text, Group } from 'react-konva';
 import styles from './App.module.css'
 
 function App() {
+  const points = [
+    { x: 620, y: 380, label: '1' },
+    { x: 590, y: 620, label: '2' },
+    { x: 420, y: 740, label: '3' },
+  ];
+
   useEffect(() => {
     window.scrollTo({
       top: 0,
@@ -12,7 +19,49 @@ function App() {
   return (
     <div className={styles.wrapper}>
       <div className={styles.content}>
-
+        <Stage width={window.innerWidth * 2} height={window.innerHeight}>
+          <Layer>
+            {points.map((point) => (
+              <Group key={point.label}>
+                <Shape
+                  x={point.x}
+                  y={point.y}
+                  width={66}
+                  height={30}
+                  sceneFunc={(context, shape) => {
+                    const width = shape.width();
+                    const height = shape.height();
+                    context.beginPath();
+                    context.ellipse(width / 2, height / 2, width / 2, height / 2, 0, 0, Math.PI * 2);
+                    context.closePath();
+                    context.fillStyle = 'rgba(234, 98, 0, 0.5)';
+                    context.shadowColor = 'rgba(0, 0, 0, 0.5)';
+                    context.shadowBlur = 20;
+                    context.shadowOffsetY = 15; 
+                    context.fill(); 
+                    context.beginPath();
+                    context.ellipse(width / 2, height / 2, width / 2, height / 2, 0, 0, Math.PI * 2);
+                    context.closePath();
+                    context.fillStyle = '#FFD24B';
+                    context.fill();
+                    context.strokeStyle = '#917F66';
+                    context.lineWidth = 1; 
+                    context.stroke();
+                    context.fillStrokeShape(shape);
+                  }}
+                />
+                <Text
+                  x={point.x + 28}
+                  y={point.y + 6}
+                  text={point.label}
+                  fontSize={20}
+                  fill="black"
+                  align="center"
+                />
+              </Group>
+            ))}
+          </Layer>
+        </Stage>
       </div>
     </div>
   );
