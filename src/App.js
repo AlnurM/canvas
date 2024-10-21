7b47ca682f8ffd4a2f9c9b38f19546f46fb0ca04
AlnurM <alnur.mustafayev@gmail.com>
2024-10-21 13:40:57 +0500

    feat: init wrapper & content plates

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import './App.css';
+import { useEffect } from 'react';
+import styles from './App.module.css'
 
 function App() {
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      left: document.body.scrollWidth,
+      behavior: "smooth",
+    });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
+    <div className={styles.wrapper}>
+      <div className={styles.content}>
+
+      </div>
     </div>
   );
 }
